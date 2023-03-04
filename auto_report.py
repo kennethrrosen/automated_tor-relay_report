e01433a6b43d9532241b7e5688ca697dbfed68b4
journoSEC <kenrosen@proton.me>
2023-03-04 07:26:08 +0100

    switched to postfix instead of mailx

--- auto_report.py
+++ auto_report.py
@@ -7,6 +7,7 @@ result = subprocess.run(nyx_command, capture_output=True, text=True)
 # Format the data
 data = result.stdout.strip()
 
-# Use mailx to send the email
-mailx_command = ['mailx', '-s', 'Nyx Data Report', '-r', 'sender@example.com', 'recipient@example.com']
-subprocess.run(mailx_command, input=data, text=True)
+# Use postfix to send the email
+postfix_command = ['sendmail', '-t']
+process = subprocess.Popen(postfix_command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+stdout, stderr = process.communicate(input='From: sender@example.com\nTo: recipient@example.com\nSubject: Nyx Data Report\n\n' + data)
