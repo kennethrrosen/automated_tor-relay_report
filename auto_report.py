a8799b1595f1ffa4f2f671e30eb0d301388b50d7
journoSEC <kenrosen@proton.me>
2023-03-05 13:50:59 +0100

    Update auto_report.py

--- auto_report.py
+++ auto_report.py
@@ -1,63 +1,51 @@
-###UNDER REVIEW
-
 import subprocess
 import datetime
+import os
 
-# Get Nyx version & stats
-nyx_command = ['nyx','-v']
-nyx_data = subprocess.run(nyx_command, capture_output=True, text=True).stdout.strip()
+def get_command_output(command, no_output_message='Nothing to show'):
+    """Runs a command and returns its output, or a default message if the output is empty."""
+    output = subprocess.run(command, capture_output=True, text=True).stdout.strip()
+    return output if output else no_output_message
+
+# Get Nyx version
+nyx_command = ['nyx', '-v']
+nyx_data = get_command_output(nyx_command, no_output_message='Unable to get Nyx version')
 
 # Get Tor stats
-stats_command = ['cat', '/var/lib/tor/stats/*']
-stats_data = subprocess.run(stats_command, capture_output=True, text=True).stdout.strip()
-if not stats_data:
-    stats_data = 'Nothing to show'
+stats_command = ['sudo', 'cat', '/var/lib/tor/stats/hidserv-v3-stats', '/var/lib/tor/stats/hidserv-stats', '/var/lib/tor/stats/dirreq-stats', '/var/lib/tor/stats/bridge-stats']
+stats_data = get_command_output(stats_command)
 
-# Check and get system logs for error messages
-log_command = ['cat', '/var/log/syslog']
-log_data = subprocess.run(log_command, capture_output=True, text=True).stdout.strip()
+# Get system logs for error messages
 log_command = ['grep', '-E', 'error|critical|fail|warning', '/var/log/syslog']
-log_data_filtered = subprocess.run(log_command, capture_output=True, text=True).stdout.strip()
-if not log_data_filtered:
-    log_data_filtered = 'Nothing to show'
+log_data_filtered = get_command_output(log_command)
 
 # Get SSH login attempts from the past 48 hours
 ssh_command = ['grep', 'sshd.*Failed', '/var/log/auth.log', '-n', f'-e "{(datetime.datetime.now() - datetime.timedelta(hours=48)).strftime("%b %d %H:%M:%S")}"']
-ssh_data = subprocess.run(ssh_command, capture_output=True, text=True).stdout.strip()
-if not ssh_data:
-    ssh_data = 'Nothing to show'
+ssh_data = get_command_output(ssh_command)
 
 # Get system updates and upgrades
-updates_command = ['apt-get', 'update']
-subprocess.run(updates_command)
 upgrade_command = ['apt-get', 'upgrade', '-s']
-upgrade_data = subprocess.run(upgrade_command, capture_output=True, text=True).stdout.strip()
-if not upgrade_data:
-    upgrade_data = 'Nothing to show'
+upgrade_data = get_command_output(upgrade_command)
 
 # Get port scanning messages
 nmap_command = ['grep', '-i', 'nmap', '/var/log/syslog']
-port_data = subprocess.run(nmap_command, capture_output=True, text=True).stdout.strip()
-if not port_data:
-    port_data = 'Nothing to show'
+port_data = get_command_output(nmap_command)
 
-# Format the data
-nyx_data = f'<h3>Nyx Version:</h3><pre>{nyx_data}</pre>'
-stats_data = '<h3>Tor Stats:</h3><pre>{}</pre>'.format(stats_data)
-log_data_filtered = f'<h3>System Logs:</h3><pre>{log_data_filtered}</pre>'
-ssh_data = f'<h3>SSH Login Attempts:</h3><pre>{ssh_data}</pre>'
-upgrade_data = f'<h3>System Updates and Upgrades:</h3><pre>{upgrade_data}</pre>'
-port_data = f'<h3>Port Scanning Messages:</h3><pre>{port_data}</pre>'
+# Save all data to a file
+output_filename = 'auto_report.txt'
+with open(output_filename, 'w') as output_file:
+    output_file.write(f'<h3>Nyx Version:</h3>\n{nyx_data}\n\n')
+    output_file.write(f'<h3>Tor Stats:</h3>\n{stats_data}\n\n')
+    output_file.write(f'<h3>System Logs:</h3>\n{log_data_filtered}\n\n')
+    output_file.write(f'<h3>SSH Login Attempts:</h3>\n{ssh_data}\n\n')
+    output_file.write(f'<h3>System Updates and Upgrades:</h3>\n{upgrade_data}\n\n')
+    output_file.write(f'<h3>Port Scanning Messages:</h3>\n{port_data}\n\n')
 
-# Send email with Nyx, SSH, system update, and port scan data
+# Send email with auto report in body
 postfix_command = ['sendmail', '-t']
+with open(output_filename, 'r') as report_file:
+    report_data = report_file.read()
+body = f'<html><body>Please find the auto report below.</body></html>\n\n{report_data}'
+message = f'From: sender@example.com\nTo: recipient@example.com\nSubject: Tor Node Report\nContent-Type: text/html\n\n{body}'
 process = subprocess.Popen(postfix_command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-body = f'<html><body>{nyx_data}<br>{stats_data}<br>{log_data_filtered}<br>{ssh_data}<br>{upgrade_data}<br>{port_data}</body></html>'
-stdout, stderr = process.communicate(input=f'From: sender@example.com\nTo: recipient@example.com\nSubject: Tor Node Report\nContent-Type: text/html\n\n{body}')
-
-
-# Send email with Nyx, SSH, system update, and port scan data
-postfix_command = ['sendmail', '-t']
-process = subprocess.Popen(postfix_command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-body = f'<html><body>{nyx_data}<br>{stats_data}<br>{log_data_filtered}<br>{ssh_data}<br>{upgrade_data}<br>{port_data}</body></html>'
-stdout, stderr = process.communicate(input=f'From: sender@example.com\nTo: recipient@example.com\nSubject: Tor Node Report\nContent-Type: text/html\n\n{body}')
+stdout, stderr = process.communicate(input=message)
